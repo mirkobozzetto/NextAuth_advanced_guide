14dc9e2cbd4da384c4b7f4cc9fd87ecacaa6ba2f
Mirko Bozzetto <mirkouch@gmail.com>
2024-01-02 20:46:03 +0100

    Update global CSS and page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div>
-      <Button size="lg" variant="default">
-        Click me
-      </Button>
-    </div>
+    <main className="flex h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"></main>
   );
 };
 
