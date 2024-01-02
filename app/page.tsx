3c15edd3e32916c59dc82ef014d19f71c9179a34
Mirko Bozzetto <mirkouch@gmail.com>
2024-01-02 21:01:09 +0100

    Update page styling and add tailwindcss/postcss7-compat dependency

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"></main>
+    <main className="flex h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+      </div>
+    </main>
   );
 };
 
